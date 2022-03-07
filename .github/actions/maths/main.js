35cf666828602e6d23364ad9e5594d36f0c368af
danielzaera <93714881+danielzaera@users.noreply.github.com>
2022-03-07 17:40:18 +0100

    Update main.js

--- .github/actions/maths/main.js
+++ .github/actions/maths/main.js
@@ -4,8 +4,8 @@ const operation-A = core.getInput("operation-A");
 const operacion-B = core.getInput("operation-B");
 const operation = core.getInput("operation");
 
-console.log( suma ${operation}`);
-console.log( suma ${operation}`);
+console.log( suma ${operation-A}`);
+console.log( suma ${operation-B}`);
 if (operation) {
   console.log( result ${env.result}`);
 }
