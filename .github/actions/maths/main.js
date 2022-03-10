2f5c75fde15d37f45b82e817f366928a9dab5293
danielzaera <93714881+danielzaera@users.noreply.github.com>
2022-03-10 16:20:45 +0100

    Update main.js

--- .github/actions/maths/main.js
+++ .github/actions/maths/main.js
@@ -1,3 +1,4 @@
+const core = require('@actions/core');
 const core = require("@actions/core");
 
 const operation-A = core.getInput("operation-A");
@@ -8,4 +9,11 @@ console.log( suma ${operation-A}`);
 console.log( suma ${operation-B}`);
 if (operation) {
   console.log( result ${env.result}`);
+}
+
+// Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
 }
